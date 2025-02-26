02a614cbc6a6e7895f4c65d221f6de76e52190e2
Mesnew <chiffonjavel@gmail.com>
2025-02-26 10:55:59 +0100

    Updated Program.cs

--- gest_abs/Program.cs
+++ gest_abs/Program.cs
@@ -2,19 +2,23 @@ using gest_abs.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using gest_abs;
 using gest_abs.Services;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Ajouter Entity Framework Core
 builder.Services.AddDbContext<GestionAbsencesContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(10, 11, 8))
     ));
+
+// Jeter la mappage par défaut des claims pour éviter toute modification du Role
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 // Ajouter l'authentification JWT
 builder.Services.AddAuthentication(options =>
@@ -32,7 +36,9 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = jwtSettings["Issuer"],
         ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"])),
+        // Spécifier le type de claim contenant le rôle
+        RoleClaimType = ClaimTypes.Role
     };
 });
 builder.Services.AddAuthorization();
@@ -92,6 +98,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/home", () => Results.Redirect("/swagger"));
+
+app.UseMiddleware<BearerPrefixMiddleware>();
 
 // Configurer le Middleware pour l'authentification et l'autorisation
 app.UseHttpsRedirection();
